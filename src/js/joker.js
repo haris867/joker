343860f5badbb518c9958eb8162a5725158d2664
Haris <haris.usman@hotmail.com>
2022-10-27 19:51:44 +0200

    Added try catch-block to function dadJoke in joker.js

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,7 +1,11 @@
-import joker from 'give-me-a-joke';
-
 export function dadJoke() {
-	return new Promise(function (resolve, reject) {
-		joker.getRandomDadJoke(resolve);
-	});
+  return new Promise(function (resolve, reject) {
+    try {
+      joker.getRandomDadJoke(function (joke) {
+        resolve(joke);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
